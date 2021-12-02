c6466ab42c8dea00931ef32e76f0d8b5a1bcf5fb
Jacob Bahn <jacobabahn@southern.edu>
2021-12-02 09:46:31 -0500

    Added the table

---
+++ constraint.js
@@ -0,0 +1,40 @@
+class Constraint {
+    constructor(v, val) {
+        this.v = v
+        this.val = val
+        this.stack = []
+    }
+
+    get = (v) => {
+        if (this.stack.length > 0) {
+            this.v.dependencies += stack[0]
+        }
+        if (!this.v.valid) {
+            this.v.valid = true
+            this.stack.push(this.v)
+            this.v.value = this.v.eval()
+            this.stack.pop()
+        }
+
+        return this.v.value
+    }
+
+    set = (v, val) => {
+        this.v.value = this.val
+        for (let dep in this.v.dependencies) {
+            if (dep.valid) {
+                invalidate(dep)
+            }
+        }
+    }
+
+    invalidate = (v) => {
+        this.v.valid = false
+        for (let dep in this.v.dependencies) {
+            if (dep.valid) {
+                invalidate(dep)
+            }
+            this.v.dependencies = []
+        }
+    }
+}
